5d8cd5b7824fd6c65bb4da8788315d56c7a8de8b
deveb1479@gmail.com <deveb1479@gmail.com>
2023-01-03 21:26:13 +0900

    start!

---
+++ CutlassC/Card/Type.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CutlassC.Card
+{
+    internal enum Type
+    {
+        Escape = 1,
+
+        Red = 2,
+        Yellow = 4,
+        Blue = 8,
+
+        Black = 16,
+        
+        Mermaid = 32,
+        Pirate = 64,
+        SkullKing = 128,
+
+        ScaryMary = 65,
+    }
+}
